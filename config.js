2efdcd73deeed567c50ecc94b6bd102ae1e4fe31
Vitalij1989 <108751440+Vitalij1989@users.noreply.github.com>
2023-05-12 16:20:36 +0300

    +

---
+++ config.js
@@ -0,0 +1,9 @@
+const DB_HOST =
+  "mongodb+srv://som:MMRQEy9rUHSPsZpS@krabaton.5mlpr.gcp.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+const HOST = 4000;
+
+module.exports = {
+  DB_HOST,
+  HOST,
+};
